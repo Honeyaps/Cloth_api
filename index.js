916fee1ac8c1e08fed8757773942589cf59650ee
Hemant Singh <honeyaps12345@gmail.com>
2024-09-23 19:49:30 +0530

    changes regarding deployment

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ app.use(
   })
 );
 app.use(express.json());
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
+
 dbconnection();
 app.get('/', (req, res) => {
   console.log('Example endpoint hit');
